bc4afffcdbf49ad5161b2abf16ac8f11ed43f9dd
Stacy Vlasits <stacyvlasits@gmail.com>
2021-11-03 12:03:47 -0500

    modified to make npm package

--- hello-app.js
+++ hello-app.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import Paper from '@mui/material/Paper';
-import Link from '@mui/material/Link';
-import { makeStyles } from '@material-ui/core/styles';
-
-
-const useStyles = makeStyles((theme) => ({
-    button: {
-      height: 36,
-      margin: theme.spacing(1),
-    },
-  }));
-  
+import Link from '@mui/material/Link';  
   
 const HelloApp = () => {
-    const classes = useStyles();
     return (
         <Paper variant="outlined">
             <h1>Hello App!</h1>
